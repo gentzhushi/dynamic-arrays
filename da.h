60bbb16c7a569680df61aaab3bfe3fb765150206
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2026-02-03 20:03:04 +0100

    eotd commit sha ropt

--- da.h
+++ da.h
@@ -10,32 +10,22 @@
 typedef struct vector_s vector_t;
 
 /**
- * Metodat e Vektorit
- */
-typedef struct vm_s {
-	void	*(*front)	(vector_t* self);
-	void	*(*back)	(vector_t* self);
-
-	void	*(*at)		(vector_t* self, size_t i);
-	
-	void	*(*pop_f)	(vector_t* self);
-	void	*(*pop_b)	(vector_t* self);
-	
-	void	(*push_f)	(vector_t* self, void* value);
-	void	(*push_b)	(vector_t* self, void* value);
-} vm_t;
-
-/**
  * Vektori
  */
 typedef struct vector_s {
-	void 			*items;
+	void 	*items;
 
-	size_t 			count;
-	size_t 			capacity;
-	size_t 			data_size;
+	size_t 	count;
+	size_t 	capacity;
+	size_t 	data_size;
 	
-	const		vm_t *methods;
+	void *(*front)(vector_t *self);
+	void *(*back)(vector_t *self);
+	void *(*at)(vector_t *self, size_t i);
+	void *(*pop_f)(vector_t *self);
+	void *(*pop_b)(vector_t *self);
+	void (*push_f)(vector_t *self, void *value);
+	void (*push_b)(vector_t *self, void *value);
 } vector_t;
 
 vector_t 	new_vector(size_t data_size); // 1 per char etj...
@@ -50,42 +40,92 @@ void		free_vector(vector_t *v);
 #ifdef DYNAMIC_ARRAY_IMPLEMENTATION
 
 #include<stdlib.h>
+#include<string.h>
 #include<stdio.h> // fshije qita ma von
 
 /**
  * Metodat
  */
-static void* v_front(vector_t *self) {
+void* v_front(vector_t *self) {
 	printf("v_front");
-	return NULL;
+
+	if (!self || self->count == 0)
+		return NULL;
+
+	return (unsigned char*)self->items;
 }
 
-static void* v_back(vector_t *self) {
+void* v_back(vector_t *self) {
 	printf("v_back");
-	return NULL;
+
+	if (!self || self->count == 0)
+		return NULL;
+	
+	return (unsigned char*) 
+		self->items + (self->count - 1) * self->data_size;
 }
 
-static void* v_at(vector_t *self, size_t i) {
+void* v_at(vector_t *self, size_t i) {
 	printf("v_at");
-	return NULL;
+	
+	if (!self || self->count == 0)
+		return NULL;
+
+	return (unsigned char*)self->items + i * self->data_size;
 }
 
-static void* v_pop_f(vector_t *self) {
+void* v_pop_f(vector_t *self) {
 	printf("v_pop_front");
-	return NULL;
+
+	if (!self || self->count == 0)
+		return NULL;
+	
+	self->count--;
+	return (unsigned char*)self->items;
 }
 
-static void* v_pop_b(vector_t *self) {
+void* v_pop_b(vector_t *self) {
 	printf("v_pop_back");
-	return NULL;
+		
+	if (!self || self->count == 0)
+		return NULL;
+		
+	return (unsigned char*)self->items + --self->count * self->data_size;
 }
 
-static void v_push_f(vector_t *self, void *value) {
-	printf("v_push_front");
+void v_push_f(vector_t *self, void *value) {
+	(void)self;
+	(void)value;
+	printf("push_front sosht i implementum.");
 }
 
-static void v_push_b(vector_t *self, void *value) {
+void v_push_b(vector_t *self, void *value) {
 	printf("v_push_back");
+
+	if (!self) return;
+
+	if (self->capacity == 0){
+		self->capacity = 256;
+	} else if (self->count >= self->capacity){
+		self->capacity *= 2;
+	}
+
+	if (self->items == NULL){
+		self->items = malloc(self->capacity * self->data_size);
+	} else if (self->count >= self->capacity){
+		void *tmp = realloc(self->items, self->capacity * self->data_size);
+		if (!tmp) {
+			perror("v_push_b");
+			return;
+		}
+		self->items = tmp;
+	}
+
+	memcpy((unsigned char*)(self->items + self->count++ * self->data_size),
+			value,
+			self->data_size
+		);
+
 }
 
 /**
@@ -95,17 +135,14 @@ vector_t new_vector(size_t data_size){
 	vector_t v = {0};
 
 	v.data_size = data_size;
-
-	v.front = v_front,
-	v.back = v_back,
 	
-	v.at = v_at,
-	
-	v.pop_f = v_pop_f,
-	v.pop_b = v_pop_b,
-
-	v.push_f = v_push_f,
-	v.push_b = v_push_b,
+	v.front = v_front;
+	v.back = v_back;
+	v.at = v_at;
+	v.pop_f = v_pop_f;
+	v.pop_b = v_pop_b;
+	v.push_f = v_push_f;
+	v.push_b = v_push_b;
 
 	return v;
 }
