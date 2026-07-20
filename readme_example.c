91b08faeaca555a2688c240207cb47f23fe0b5d9
Gent Zhushi <gentzhushi01@gmail.com>
2026-07-20 11:05:27 +0200

    Added readme example, and removed debug artifact.

--- readme_example.c
+++ readme_example.c
@@ -1,9 +1,3 @@
-# STB-style dynamic arrays library in C
-
-This is a single-file project. In order to use it and its implementations, just copy paste this file to your project, and include it. In order to use the implementation, `#define DYNAMIC_ARRAY_IMPLEMENTATION` before you include it.
-
-**Example usage**
-```c
 #include <stdio.h>
 
 #define DYNAMIC_ARRAYS_IMPLEMENTATION
@@ -22,7 +16,3 @@ int main(void)
 
         da_free(alphabet);
 }
-```
----
-You should see the following output:
-
--- test.c
+++ test.c
@@ -7,15 +7,11 @@ int main(void)
 {
         float *farr = NULL;
 
-        fprintf(stderr, "Trying to append...\n");
-
         for (int i = 0; i < 300; i++)
                 da_append(farr, (float)i);
 
-        fprintf(stderr, "Trying to print...\n");
-
         for (int i = 0; i < da_len(farr); i++)
-                fprintf(stderr, "%f\n", farr[i]);
+                printf("%f\n", farr[i]);
 
         da_free(farr);
         return 0;
